17c1dbe2ff48b8b26dc88272d36bd378fd3d930c
Ashraf Abbas <ashrafabbasramadan@GMAIL.COM>
2024-07-30 01:04:03 +0300

    Add project files.

---
+++ Versioning/Command.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows;
+
+namespace Versioning
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            string revitVeriosn = null;
+
+#if Revit_2023
+            revitVeriosn = "2023";
+#elif Revit_2022
+            revitVeriosn = "2022";
+#endif
+
+            MessageBox.Show($"This line of code is for Revit {revitVeriosn}",
+                             "Multi-Version",
+                             MessageBoxButton.OK,
+                             MessageBoxImage.Information);
+
+            return Result.Succeeded;
+        }
+    }
+}
